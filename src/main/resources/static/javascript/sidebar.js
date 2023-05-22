00eee115b190274573ffab60deca5c2c38e977e1
MarinoVranic <marino.vranic@gmail.com>
2023-05-22 22:49:46 +0200

    Implemented navigation bar to lokacija, dobavljac and vrsta uredaja

---
+++ src/main/resources/static/javascript/sidebar.js
@@ -0,0 +1,16 @@
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.addEventListener("click", closeSidebarOutside);
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.removeEventListener("click", closeSidebarOutside);
+}
+
+function closeSidebarOutside(event) {
+    var sidebar = document.getElementById("mySidebar");
+    if (!sidebar.contains(event.target) && event.target !== document.getElementById("openNav")) {
+        w3_close();
+    }
+}
