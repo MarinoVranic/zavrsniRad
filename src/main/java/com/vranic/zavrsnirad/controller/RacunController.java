53ca67e16eb4548cf7040458251318af1ec5dea8
MarinoVranic <marino.vranic@gmail.com>
2023-05-27 17:43:02 +0200

    Closing repairingRacun for now.

--- src/main/java/com/vranic/zavrsnirad/controller/RacunController.java
+++ src/main/java/com/vranic/zavrsnirad/controller/RacunController.java
@@ -1,7 +1,127 @@
+//package com.vranic.zavrsnirad.controller;
+//
+//
+//import com.vranic.zavrsnirad.model.Lokacija;
+//import com.vranic.zavrsnirad.model.Racun;
+//import com.vranic.zavrsnirad.service.RacunService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//
+//
+//import java.util.List;
+//
+//import java.net.URLEncoder;
+//import java.net.URLDecoder;
+//import java.io.UnsupportedEncodingException;
+//
+//@Controller
+//@RequestMapping("/racun")
+//public class RacunController {
+//    @Autowired
+//    private RacunService racunService;
+//
+//    @GetMapping("/all")
+//    public String getAllRacun(Model model) {
+//        model.addAttribute("sviRacuni", racunService.getAllRacun());
+//        return "racun/racun";
+//    }
+//
+//    @GetMapping("/{brojRacuna}")
+//    public Racun getRacunById(@PathVariable String brojRacuna) {
+//        String decodedBrojRacuna = decodeURLComponent(brojRacuna);
+//
+//        return racunService.getRacunById(decodedBrojRacuna);
+//    }
+//
+//    @GetMapping("/update/{brojRacuna}")
+//    public String updateLokacija(@PathVariable(value = "brojRacuna") String brojRacuna, Model model) {
+//        String decodedBrojRacuna = decodeURLComponent(brojRacuna);
+//
+//        Racun racun = racunService.getRacunById(decodedBrojRacuna);
+//        model.addAttribute("racun", racun);
+//        return "lokacija/updateLokacija";
+//    }
+//
+//    // ...
+//
+//    @PostMapping("/addNew")
+//    public String addLokacija(@ModelAttribute("lokacija") Racun racun, Model model) {
+//        String encodedBrojRacuna = encodeURLComponent(racun.getBrojRacuna());
+//
+//        if (racunService.checkIfBrojRacunaIsAvailable(encodedBrojRacuna) != 0) {
+//            model.addAttribute("error", "Račun pod tim brojem već postoji!");
+//            return "racun/newRacun";
+//        } else {
+//            racun.setBrojRacuna(encodedBrojRacuna);
+//            racunService.save(racun);
+//        }
+//        return "redirect:/racun/all";
+//    }
+//
+//    @PostMapping("/save")
+//    public String saveLokacija(@ModelAttribute("racun") Racun racun, Model model) {
+//        String encodedBrojRacuna = encodeURLComponent(racun.getBrojRacuna());
+//
+//        if (racunService.checkIfBrojRacunaIsAvailable(encodedBrojRacuna) != 0) {
+//            model.addAttribute("error", "Račun pod tim brojem već postoji!");
+//            return "racun/updateRacun";
+//        }
+//        racun.setBrojRacuna(encodedBrojRacuna);
+//        racunService.save(racun);
+//        return "redirect:/racun/all";
+//    }
+//
+//    @GetMapping("delete/{brojRacuna}")
+//    public String deleteById(@PathVariable(value = "brojRacuna") String brojRacuna) {
+//        String decodedBrojRacuna = decodeURLComponent(brojRacuna);
+//
+//        racunService.deleteById(decodedBrojRacuna);
+//        return "redirect:/racun/all";
+//    }
+//
+//    @GetMapping("/find")
+//    public String findDobavljacByName(@RequestParam("brojRacuna") String brojRacuna, Model model) {
+//        String decodedBrojRacuna = decodeURLComponent(brojRacuna);
+//
+//        List<Racun> racunList = racunService.findRacunByBrojRacuna(decodedBrojRacuna);
+//        if (racunList.isEmpty()) {
+//            model.addAttribute("error", "Račun pod tim brojem ne postoji u sustavu!");
+//            model.addAttribute("sviRacuni", racunService.getAllRacun());
+//            Racun racun = new Racun();
+//            model.addAttribute("racun", racun);
+//        } else {
+//            model.addAttribute("sviRacuni", racunList);
+//            Racun racun = new Racun();
+//            model.addAttribute("racun", racun);
+//        }
+//        return "racun/racun";
+//    }
+//
+//    // Utility method to encode URL component
+//    private String encodeURLComponent(String component) {
+//        try {
+//            return URLEncoder.encode(component, "UTF-8");
+//        } catch (UnsupportedEncodingException e) {
+//            e.printStackTrace();
+//            return component;
+//        }
+//    }
+//
+//    // Utility method to decode URL component
+//    private String decodeURLComponent(String component) {
+//        try {
+//            return URLDecoder.decode(component, "UTF-8");
+//        } catch (UnsupportedEncodingException e) {
+//            e.printStackTrace();
+//            return component;
+//        }
+//    }
+//}
 package com.vranic.zavrsnirad.controller;
 
 
-import com.vranic.zavrsnirad.model.Lokacija;
 import com.vranic.zavrsnirad.model.Racun;
 import com.vranic.zavrsnirad.service.RacunService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,60 +139,34 @@ public class RacunController {
     private RacunService racunService;
 
     @GetMapping("/all")
-    public String getAllRacun(Model model) {
+    public String getAllRacun(Model model){
         model.addAttribute("sviRacuni", racunService.getAllRacun());
+        Racun racun = new Racun();
+        model.addAttribute("racun", racun);
         return "racun/racun";
     }
 
-    @GetMapping("/{brojRacuna}")
-    public Racun getRacunById(@PathVariable String brojRacuna) {
-        return racunService.getRacunById(brojRacuna);
-    }
-
-    @GetMapping("/update/{brojRacuna}")
-    public String updateLokacija(@PathVariable(value = "brojRacuna") String brojRacuna, Model model) {
-        Racun racun = racunService.getRacunById(brojRacuna);
+    @PostMapping("/update")
+    public String updateRacun(Racun racun, Model model) {
+        racunService.save(racun);
         model.addAttribute("racun", racun);
-        return "lokacija/updateLokacija";
-    }
-
-    @GetMapping("/addNew")
-    public String addNewLokacija(Model model){
-        Racun racun = new Racun();
-        model.addAttribute("lokacija", racun);
-        return "racun/newRacun";
-    }
-
-    @PostMapping("/addNew")
-    public String addLokacija(@ModelAttribute("lokacija") Racun racun, Model model) {
-        if(racunService.checkIfBrojRacunaIsAvailable(racun.getBrojRacuna())!=0){
-            model.addAttribute("error", "Račun pod tim brojem već postoji!");
-            return "racun/newRacun";
-        }else {
-            racunService.save(racun);
-        }
         return "redirect:/racun/all";
     }
 
     @PostMapping("/save")
-    public String saveLokacija(@ModelAttribute("racun") Racun racun, Model model) {
-        if(racunService.checkIfBrojRacunaIsAvailable(racun.getBrojRacuna())!=0)
-        {
-            model.addAttribute("error", "Račun pod tim brojem već postoji!");
-            return "racun/updateRacun";
-        }
+    public String saveRacun(@ModelAttribute("racun") Racun racun) {
         racunService.save(racun);
         return "redirect:/racun/all";
     }
 
-    @GetMapping("delete/{brojRacuna}")
-    public String deleteById(@PathVariable(value = "brojRacuna") String brojRacuna) {
+    @GetMapping("/delete")
+    public String deleteById(String brojRacuna) {
         racunService.deleteById(brojRacuna);
         return "redirect:/racun/all";
     }
 
     @GetMapping("/find")
-    public String findDobavljacByName(@RequestParam("brojRacuna") String brojRacuna, Model model) {
+    public String findRacunByName(@RequestParam("brojRacuna") String brojRacuna, Model model) {
         List<Racun> racunList = racunService.findRacunByBrojRacuna(brojRacuna);
         if (racunList.isEmpty()) {
             model.addAttribute("error", "Račun pod tim brojem ne postoji u sustavu!");
