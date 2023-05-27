1608880bbb944d06f28bd1a9c615a3b2b7b865c6
MarinoVranic <marino.vranic@gmail.com>
2023-05-27 14:16:46 +0200

    Implemented validation for Vrsta uredaja. Closes #24

--- src/main/java/com/vranic/zavrsnirad/controller/VrstaUredajaController.java
+++ src/main/java/com/vranic/zavrsnirad/controller/VrstaUredajaController.java
@@ -1,5 +1,6 @@
 package com.vranic.zavrsnirad.controller;
 
+import com.vranic.zavrsnirad.model.Dobavljac;
 import com.vranic.zavrsnirad.model.Lokacija;
 import com.vranic.zavrsnirad.model.VrstaUredaja;
 import com.vranic.zavrsnirad.service.VrstaUredajaService;
@@ -17,48 +18,70 @@ public class VrstaUredajaController {
     private VrstaUredajaService vrstaUredajaService;
 
     @GetMapping("/all")
-    public String getAllVrstaUredaja(Model model){
+    public String getAllVrstaUredaja(Model model) {
         model.addAttribute("sveVrsteUredaja", vrstaUredajaService.getAllVrstaUredaja());
-        VrstaUredaja vrstaUredaja = new VrstaUredaja();
-        model.addAttribute("vrstauredaja", vrstaUredaja);
         return "vrstaUredaja/vrstaUredaja";
     }
+
     @GetMapping("/{id}")
-    public VrstaUredaja getVrstaUredajaById(@PathVariable Long id){
+    public VrstaUredaja getVrstaUredajaById(@PathVariable Long id) {
         return vrstaUredajaService.getVrstaUredajaById(id);
     }
-    @PostMapping("/update")
-    public String updateVrstaUredaja(VrstaUredaja vrstaUredaja, Model model){
-        vrstaUredajaService.save(vrstaUredaja);
-        model.addAttribute("vrstauredaja",vrstaUredaja);
+
+    @GetMapping("/update/{id}")
+    public String updateVrstaUredaja(@PathVariable(value = "id") Long id, Model model) {
+        VrstaUredaja vrstaUredaja = vrstaUredajaService.getVrstaUredajaById(id);
+        model.addAttribute("vrstaUredaja", vrstaUredaja);
+        return "vrstaUredaja/updateVrstaUredaja";
+    }
+
+    @GetMapping("/addNew")
+    public String addNewVrstaUredaja(Model model){
+        VrstaUredaja vrstaUredaja = new VrstaUredaja();
+        model.addAttribute("vrstaUredaja", vrstaUredaja);
+        return "vrstaUredaja/newVrstaUredaja";
+    }
+
+    @PostMapping("/addNew")
+    public String addVrstaUredaja(@ModelAttribute("vrstaUredaja") VrstaUredaja vrstaUredaja, Model model) {
+        if(vrstaUredajaService.checkIfNazivVrsteUredajaIsAvailable(vrstaUredaja.getNazivVrsteUredaja())!=0){
+            model.addAttribute("error", "Vrsta uređaja tog naziva već postoji!");
+            return "vrstaUredaja/newVrstaUredaja";
+        }else {
+            vrstaUredajaService.save(vrstaUredaja);
+        }
         return "redirect:/vrstaUredaja/all";
     }
 
     @PostMapping("/save")
-    public String saveVrstaUredaja(@ModelAttribute("vrstauredaja") VrstaUredaja vrstaUredaja){
+    public String saveVrstaUredaja(@ModelAttribute("vrstaUredaja") VrstaUredaja vrstaUredaja, Model model) {
+        if(vrstaUredajaService.checkIfNazivVrsteUredajaIsAvailable(vrstaUredaja.getNazivVrsteUredaja())!=0)
+        {
+            model.addAttribute("error", "Vrsta uređaja tog naziva već postoji!");
+            return "vrstaUredaja/updateVrstaUredaja";
+        }
         vrstaUredajaService.save(vrstaUredaja);
         return "redirect:/vrstaUredaja/all";
     }
 
-    @GetMapping("/delete/{id}")
-    public String deleteVrstaUredajaById(@PathVariable(value = "id") Long id){
+    @GetMapping("delete/{id}")
+    public String deleteById(@PathVariable(value = "id") Long id) {
         vrstaUredajaService.deleteById(id);
         return "redirect:/vrstaUredaja/all";
     }
 
     @GetMapping("/find")
-    public String findDobavljacByName(@RequestParam("nazivVrsteUredaja") String nazivVrsteUredaja, Model model) {
+    public String findVrstaUredajaByName(@RequestParam("nazivVrsteUredaja") String nazivVrsteUredaja, Model model) {
         List<VrstaUredaja> vrstaUredajaList = vrstaUredajaService.findVrstaUredajaByName(nazivVrsteUredaja);
         if (vrstaUredajaList.isEmpty()) {
             model.addAttribute("error", "Vrsta uređaja tog naziva ne postoji u sustavu!");
             model.addAttribute("sveVrsteUredaja", vrstaUredajaService.getAllVrstaUredaja());
             VrstaUredaja vrstaUredaja = new VrstaUredaja();
-            model.addAttribute("vrstauredaja", vrstaUredaja);
+            model.addAttribute("vrstaUredaja", vrstaUredaja);
         } else {
             model.addAttribute("sveVrsteUredaja", vrstaUredajaList);
             VrstaUredaja vrstaUredaja = new VrstaUredaja();
-            model.addAttribute("vrstauredaja", vrstaUredaja);
-//            System.out.println(vrstaUredajaList.get(0));
+            model.addAttribute("vrstaUredaja", vrstaUredaja);
         }
         return "vrstaUredaja/vrstaUredaja";
     }
