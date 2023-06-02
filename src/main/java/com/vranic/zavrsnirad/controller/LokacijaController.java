1e923301fcb18e1b20679f97b942325813d8bafe
MarinoVranic <marino.vranic@gmail.com>
2023-06-02 21:46:17 +0200

    Created printing report to PDF file for Lokacija. Closes #61

--- src/main/java/com/vranic/zavrsnirad/controller/LokacijaController.java
+++ src/main/java/com/vranic/zavrsnirad/controller/LokacijaController.java
@@ -1,12 +1,21 @@
 package com.vranic.zavrsnirad.controller;
 
+import com.itextpdf.io.image.ImageDataFactory;
+import com.itextpdf.layout.property.TextAlignment;
+import com.itextpdf.layout.property.VerticalAlignment;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.*;
 import com.vranic.zavrsnirad.model.Lokacija;
 import com.vranic.zavrsnirad.service.LokacijaService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
+import com.itextpdf.layout.element.Text;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -83,4 +92,93 @@ public class LokacijaController {
         }
         return "lokacija/lokacija";
     }
+
+    @GetMapping("/generatePDF")
+    public void generatePDF(HttpServletResponse response) throws IOException, DocumentException {
+        // Set the content type and attachment header
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition", "attachment; filename=\"lokacije.pdf\"");
+
+        // Create a new PDF document
+        Document document = new Document(PageSize.A4);
+
+        // Create a PdfWriter instance to write the document to the response output stream
+        PdfWriter.getInstance(document, response.getOutputStream());
+
+        // Open the document
+        document.open();
+
+//        // Create the header text
+//        Phrase headerText = new Phrase("Izvještaj o lokacijama", FontFactory.getFont(FontFactory.HELVETICA_BOLD, 14));
+//
+//        // Create the header paragraph
+//        Paragraph header = new Paragraph(headerText);
+//        document.add(header);
+        Paragraph header = new Paragraph();
+        Phrase headerPhrase = new Phrase("Izvještaj o lokacijama");
+        Font boldFont = new Font(Font.FontFamily.HELVETICA, 14, Font.BOLD);
+        headerPhrase.setFont(boldFont);
+        header.add(headerPhrase);
+        header.setAlignment(Element.ALIGN_CENTER);
+        header.setSpacingAfter(20); // Adjust the value as per your requirement
+        document.add(header);
+        // Set the font for Croatian characters
+        Font croatianFont = FontFactory.getFont("/static/fonts/arial.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+
+        // Create a table with 2 columns
+        PdfPTable table = new PdfPTable(2);
+
+        // Set the table width as a percentage of the available page width
+        table.setWidthPercentage(50);
+
+        // Set the data cell style
+        PdfPCell dataCell = new PdfPCell();
+        dataCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+
+        // Set table header cell styles
+        Font headerFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+        headerFont.setColor(BaseColor.WHITE);
+        PdfPCell headerCell = new PdfPCell();
+        headerCell.setBackgroundColor(BaseColor.DARK_GRAY);
+        headerCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        headerCell.setPadding(5);
+
+        // Add table header cells
+        headerCell.setPhrase(new Phrase("ID lokacije", headerFont));
+        table.addCell(headerCell);
+        headerCell.setPhrase(new Phrase("Naziv lokacije", headerFont));
+        table.addCell(headerCell);
+//        headerCell.setPhrase(new Phrase("", headerFont));
+//        table.addCell(headerCell);
+//        headerCell.setPhrase(new Phrase("", headerFont));
+//        table.addCell(headerCell);
+
+        // Get the list of Lokacija objects from your service
+        List<Lokacija> lokacije = lokacijaService.getAllLokacija();
+
+        // Add data cells to the table
+        for (Lokacija lokacija : lokacije) {
+            dataCell.setPhrase(new Phrase(String.valueOf(lokacija.getIdLokacije()), croatianFont));
+            table.addCell(dataCell);
+            dataCell.setPhrase(new Phrase(lokacija.getNazivLokacije(), croatianFont));
+            table.addCell(dataCell);
+        }
+
+        // Add the table to the document
+        document.add(table);
+
+        String imagePath = "static/images/AITAC values challenges solutions (1).png"; // Relative path to the image file
+        Resource resource = new ClassPathResource(imagePath);
+        Image image = Image.getInstance(resource.getURL());
+        // Set the desired width and height of the image in points (1 point = 1/72 inch)
+        float desiredWidth = 200f;
+        float desiredHeight = 100f;
+        image.scaleToFit(desiredWidth, desiredHeight);
+        document.add(image);
+
+        // Close the document
+        document.close();
+    }
+
+
 }
