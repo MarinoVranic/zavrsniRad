c7b8254df706ed3ae384b062437a314f5f5fd533
MarinoVranic <marino.vranic@gmail.com>
2023-06-05 00:18:49 +0200

    Customizing index page.

--- src/main/resources/static/javascript/sidebar.js
+++ src/main/resources/static/javascript/sidebar.js
@@ -1,10 +1,12 @@
 function w3_open() {
     document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
     document.addEventListener("click", closeSidebarOutside);
 }
 
 function w3_close() {
     document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
     document.removeEventListener("click", closeSidebarOutside);
 }
 
