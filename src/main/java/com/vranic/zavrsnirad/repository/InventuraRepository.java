c134e8739234e2fbc969aaee1d23ec0351755ffc
MarinoVranic <marino.vranic@gmail.com>
2023-12-01 14:39:08 +0100

    Adding changes to repository of Inventura

--- src/main/java/com/vranic/zavrsnirad/repository/InventuraRepository.java
+++ src/main/java/com/vranic/zavrsnirad/repository/InventuraRepository.java
@@ -12,4 +12,10 @@ public interface InventuraRepository extends JpaRepository<Inventura, Long> {
 
     @Query(value = "SELECT * FROM inventura i ORDER BY i.ID_inventure ASC", nativeQuery = true)
     List<Inventura> findALl();
+
+    @Query(value = "SELECT * FROM inventura i WHERE i.ID_inventure = :idInventure", nativeQuery = true)
+    List<Inventura> findByIdInventure(Long idInventure);
+
+    @Query(value = "SELECT COUNT(i.ID_inventure) FROM inventura i WHERE i.ID_inventure = :idInventure", nativeQuery = true)
+    Long checkIfInventuraExists(Long idInventure);
 }
