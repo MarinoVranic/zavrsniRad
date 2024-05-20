86fde2a720d9f3e794a25294d0d785dc7985db96
MarinoVranic <marino.vranic@gmail.com>
2024-05-20 13:01:17 +0200

    Changed the search method for Dobavljac, from strict name to any similar name.

--- src/main/java/com/vranic/zavrsnirad/repository/DobavljacRepository.java
+++ src/main/java/com/vranic/zavrsnirad/repository/DobavljacRepository.java
@@ -9,7 +9,8 @@ import java.util.List;
 
 @Repository
 public interface DobavljacRepository extends JpaRepository<Dobavljac, Long> {
-    @Query(value = "SELECT * FROM dobavljac d WHERE d.Naziv_dobavljaca = :nazivDobavljaca", nativeQuery = true)
+
+    @Query(value = "SELECT dobavljac FROM Dobavljac dobavljac WHERE dobavljac.nazivDobavljaca LIKE CONCAT('%', :nazivDobavljaca, '%')")
     List<Dobavljac> findByNazivDobavljaca(String nazivDobavljaca);
 
     @Query(value = "SELECT * FROM dobavljac d ORDER BY d.ID_dobavljaca ASC", nativeQuery = true)
