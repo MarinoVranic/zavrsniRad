aeebe927e581714fb1e5158c9c9d09d39e50b07e
MarinoVranic <marino.vranic@gmail.com>
2024-06-12 10:36:09 +0200

    Fixed null bug for lokacija in inventar.

--- src/main/resources/static/javascript/hidedivInventar.js
+++ src/main/resources/static/javascript/hidedivInventar.js
@@ -3,3 +3,15 @@ function hidedivInventar() {
     document.location.href = '/inventar/all';
 }
 setTimeout("hidedivInventar()", 1200);
+
+function hidedivInventar2() {
+    document.getElementById("error-message2").style.visibility = "hidden";
+    document.location.href = '/inventar/all';
+}
+setTimeout("hidedivInventar2()", 1200);
+
+function hidedivInventar3() {
+    document.getElementById("error-message3").style.visibility = "hidden";
+    document.location.href = '/inventar/all';
+}
+setTimeout("hidedivInventar3()", 1200);
